39539c3c628a15f9d6d1fab2d98c0c36c08c596c
Federico N. Deraiopian <federicoderaiopian@gmail.com>
2023-10-08 20:41:42 -0300

    avances generales sobre el proyecto

--- script.js
+++ script.js
@@ -1,3 +1,98 @@
+const testimonios = [
+    {
+        nombre: "Federico Deraiopian",
+        texto: "Muy recomendable, yo voy con ella por nutrición deportiva y realmente me esta ayudando a conseguir mi objetivo!! Muy buen trato y amabilidad",
+        destacado: true
+    },
+    {
+        nombre: "Micaela Meza",
+        texto: "Execelente y Amable. Muy profesional en todo",
+        destacado: false
+    },
+    {
+        nombre: "Geraldine Meza",
+        texto: "Excelente profesional. Muy satisfecha!",
+        destacado: true
+    },
+    {
+        nombre: "Sol Li",
+        texto: "Super recomendable. Excelente profesiona!",
+        destacado: false
+    },
+    {
+        nombre: "Cabrera Luciana",
+        texto: "Super recomendable, excelente trato y muy buena la atención!!",
+        destacado: true
+    },
+    {
+        nombre: "Daniela Zaccarello",
+        texto: "Excelente profesional, la súper recomiendo!",
+        destacado: true
+    },
+    {
+        nombre: "Jano Catriel",
+        texto: "Excelente profesional, me ayudó a mejorar mi alimentación. La recomiendo!",
+        destacado: false
+    },
+    {
+        nombre: "Federico Chinestradd",
+        texto: "Muy buena atencion! Me ayudo a lograr mis objetivos esteticos y de salud!",
+        destacado: false
+    },
+    {
+        nombre: "Sofia Brenna",
+        texto: "Muy buena profesional, súper recomendable",
+        destacado: true
+    },
+    {
+        nombre: "Faku Andujar",
+        texto: "El trato que me ofreció es de otro nivel!!! Súper profesional, recomendadísima…",
+        destacado: true
+    },
+    {
+        nombre: "Nico Peix",
+        texto: "Excelente profesional! 100% recomendable !",
+        destacado: true
+    },
+    {
+        nombre: "lorena martinez",
+        texto: "Excelente atención!!",
+        destacado: true
+    },
+    {
+        nombre: "Leticia Anchieta",
+        texto: "Excelente!!❤️",
+        destacado: true
+    },
+    {
+        nombre: "Francisco gutierrez",
+        texto: "Excelente profesional!",
+        destacado: false
+    },
+    {
+        nombre: "Norma Orioli",
+        texto: "excelente profesional",
+        destacado: true
+    },
+];
+
+const testimoniosDestacados = testimonios.filter(testimonio => testimonio.destacado);
+
+function mostrarTestimonios() {
+const testimoniosContainer = document.getElementById('testimonios');
+
+testimoniosContainer.innerHTML = '';
+
+testimoniosDestacados.forEach(testimonio => {
+    const testimonioElement = document.createElement('div');
+    testimonioElement.classList.add('testimonio');
+    testimonioElement.innerHTML = `<p>${testimonio.texto}</p> <h5 class="nombre">${testimonio.nombre}</h5>`;
+    testimoniosContainer.appendChild(testimonioElement);
+    });
+}
+
+window.addEventListener('load', mostrarTestimonios);
+
 let info = prompt ("Por favor, ingese su nombre")
 
 function saludar() {
